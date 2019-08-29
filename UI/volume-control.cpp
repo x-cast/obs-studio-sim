f148a3d128eeb313153ada78d664e12e600bfae8
pkv <pkv.stream@gmail.com>
2019-08-29 23:34:53 +0200

    UI: Enlarge volume strips

--- UI/volume-control.cpp
+++ UI/volume-control.cpp
@@ -477,7 +477,7 @@ VolControl::VolControl(OBSSource source_, bool *mutePtr, bool showConfig,
 			rec->setProperty("themeID", "MacOnly");
 		}
 #endif
-		setMaximumWidth(110);
+		setMaximumWidth(130);
 	} else {
 		QHBoxLayout *volLayout = new QHBoxLayout;
 		QHBoxLayout *textLayout = new QHBoxLayout;
